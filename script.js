b352d878c4fbc123193af64c9f4bd476447df813
saditaaja <100909193+saditaaja@users.noreply.github.com>
2022-03-08 13:15:59 +0200

    klassrsdes

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const vardi=['Jānis Ozols','Elīna Zepa', 'Alise Upīte', 'Andris Ragavs', 'Nauris Aleksējevs'];
-const balvas=['Mašīna','Ledusskapis','Mopēds','Dators','Televizors'];
-const naudaKopa= 500000;
-let rindas=document.querySelector('.rindas');
+const vardi = ['Jānis Ozols', 'Elīna Zepa', 'Alise Upīte', 'Andris Ragavs', 'Nauris Aleksējevs'];
+const balvas = ['Mašīna', 'Ledusskapis', 'Mopēds', 'Dators', 'Televizors'];
+const naudaKopa = 500000;
+let uzvaretajuSkaits = 5;
+let rindas = document.querySelector('.rindas');
+rindas.innerHTML = ' ';
+for (let i = 0; i < uzvaretajuSkaits; i++) {
+    let rand = Math.random() * vardi.length
+    rand = Math.floor(rand);
 
-let uzvaretajuSkaits= 5;
-for (let i=0;i<uzvaretajuSkaits;i++){
-let rand=Math.random()*vardi.length
-rand= Math.floor(rand);
-let uzvaretajs=vardi[rand];
-rindas.innerHTMLHTML+=`
-<tr>
-<td>${i+1}</td>
-<td>${uzvaretajs}</td>
-</tr>`
+    let uzvaretajs = vardi[rand];
+
+    rindas.innerHTML +=`
+
+        <tr>
+            <td>${i + 1}</td>
+            <td>${uzvaretajs}</td>
+            <td>${balvas}</td>
+        </tr>`
 
 console.log(vardi[rand]);
 }
