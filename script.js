c91f1b393f03b6ec5493b0d1a7da265d6e2433d9
saditaaja <100909193+saditaaja@users.noreply.github.com>
2022-03-08 11:14:34 +0200

    stundas darbs

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const vardi=['Jānis Ozols','Elīna Zepa', 'Alise Upīte', 'Andris Ragavs', 'Nauris Aleksējevs'];
 const balvas=['Mašīna','Ledusskapis','Mopēds','Dators','Televizors'];
 const naudaKopa= 500000;
-let uzvaretajuSkaits= 5;
+let rindas=document.querySelector('.rindas');
 
+let uzvaretajuSkaits= 5;
+for (let i=0;i<uzvaretajuSkaits;i++){
+let rand=Math.random()*vardi.length
+rand= Math.floor(rand);
+let uzvaretajs=vardi[rand];
 console.log(vardi[rand]);
+}
+rindas.innerHTML+=`//pievieno +, lai pieskaitītu nākamo
