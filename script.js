6fa60533adfa5699ca7aea2ba75ea7b53acfc616
saditaaja <100909193+saditaaja@users.noreply.github.com>
2022-03-08 10:18:24 +0200

    summeri

---
+++ script.js
@@ -0,0 +1,6 @@
+const vardi=['Jānis Ozols','Elīna Zepa', 'Alise Upīte', 'Andris Ragavs', 'Nauris Aleksējevs'];
+const balvas=['Mašīna','Ledusskapis','Mopēds','Dators','Televizors'];
+const naudaKopa= 500000;
+let uzvaretajuSkaits= 5;
+
+console.log(vardi[rand]);
