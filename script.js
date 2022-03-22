29dc82f6921d20fa7041ee1ae302c6d7d398fd19
saditaaja <100909193+saditaaja@users.noreply.github.com>
2022-03-22 11:10:36 +0200

    final

--- script.js
+++ script.js
@@ -2,22 +2,30 @@ const vardi = ['Jānis Ozols', 'Elīna Zepa', 'Alise Upīte', 'Andris Ragavs', '
 const balvas = ['Mašīna', 'Ledusskapis', 'Mopēds', 'Dators', 'Televizors'];
 const naudaKopa = 500000;
 let uzvaretajuSkaits = 5;
+let balvuskaits = 5;
 let rindas = document.querySelector('.rindas');
 rindas.innerHTML = ' ';
+let balvas1 = document.querySelector('.balvas');
+balvas.innerHTML = ' ';
 for (let i = 0; i < uzvaretajuSkaits; i++) {
     let rand = Math.random() * vardi.length
     rand = Math.floor(rand);
-
     let uzvaretajs = vardi[rand];
+
+    let rand1 = Math.random() * balvas.length
+    rand1 = Math.floor(rand1);
+
+    let uzvaretajs1 = balvas[rand1];
+
 
     rindas.innerHTML +=`
 
         <tr>
             <td>${i + 1}</td>
             <td>${uzvaretajs}</td>
-            <td>${balvas}</td>
+            <td>${uzvaretajs1}</td>
         </tr>`
 
-console.log(vardi[rand]);
+
 }
 
